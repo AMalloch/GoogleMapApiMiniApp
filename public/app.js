bff48910f89c8ce7765804ecfef419a0a52a0d89
Angus Malloch <angusbass@hotmail.com>
2018-04-10 17:36:03 +0100

    Added a MapWrapper class, created a new instance of a MapWrapper in app.js and in index.html used script to pass them to the browser.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
 const initialize = function(){
 
     const container = document.getElementById('main-map');
+    const center = {lat: -50, lng: 160};
+    const zoom = 14;
 
-}
+const map = new MapWrapper(container, center, zoom);
+
+};
+
+window.addEventListener('DOMContentLoaded', initialize);
